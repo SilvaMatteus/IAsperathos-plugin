ecc5c32a40e52d3c3ec20a3e9d3b6b03294c8e32
Ionésio Costa <ionesio@lsd.ufcg.edu.br>
2018-08-03 17:59:28 -0300

    Fix little bugs and code refactory

--- controller_plugin/controller_plugin.py
+++ controller_plugin/controller_plugin.py
@@ -1,16 +1,19 @@
 import requests
-from math import log
+from math import log10
 import json
 
 class IAsperathosController(object):
     
-    def __init__(self, progress_limit):
-        self.__progress_limit = progress_limit
+    def __init__(self, time_limit):
+        self.__time_limit = time_limit
     
-    def expected_progress_function(self,time_spent):
-        return math.log10(time_spent)
+    def __get_fitness_percentage(self,real_fitness, fitness_limit):
+        return log10(real_fitness) / log10(fitness_limit)
+    
+    def __get_time_spent_percentage(self, real_time, time_limit):
+        return float(real_time) / time_limit
 
-    def get_progress(self,url):
+    def __get_fitness(self,url):
         try:
             r = requests.get(url)
             progress = json.loads(r.json)
@@ -18,10 +21,11 @@ class IAsperathosController(object):
         except:
             return None
 
-    def evaluate_efficiency(self,url, time_spent): 
-        real_progress = get_progress(url)
-        if not real_progress:
+    def evaluate_efficiency(self, url, fitness_limit,time_spent): 
+        real_fitness = 0.2
+        #real_fitness = self.__get_fitness(url)
+        if not real_fitness:
             return None
-        progress_percentage = real_progress / self.progress_limit;
-        expected_progress = expected_progress_function(time_spent)
-        return progress_percentage - expected_progress
+        fitness_percentage = self.__get_fitness_percentage(real_fitness, fitness_limit)
+        time_percentage = self.__get_time_spent_percentage(time_spent, self.__time_limit)
+        return fitness_percentage - time_percentage
